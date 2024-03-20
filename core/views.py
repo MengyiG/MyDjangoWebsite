1e34814085ae3fa9a485d6953a716013e83701c4
mengyi <yiii9292@gmail.com>
2024-03-20 19:33:29 -0400

    organize templates

--- core/views.py
+++ core/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': 'Room 1'},
+    {'id': 2, 'name': 'Room 2'},
+    {'id': 3, 'name': 'Room 3'},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'rooms': rooms
+    }
+    return render(request, 'core/home.html', context)
 
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'core/room.html')
